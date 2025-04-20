938b913516ccfcac91c92442136b8b233a9bf487
RH-o666 <127748418+minute124@users.noreply.github.com>
2025-04-20 18:01:47 +0900

    Update and rename KeySystem to KeySystem.lua

---
+++ KeySystem.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        key = "AUYKI1-DS7FM4-3QE38",
+        hwid = "C5C505AF-163A-448B-B034-CD6BEEF4F652",
+        description = "Hajin lifetime"
+    },
+    {
+        key = "LASDIO-AS2DSA-3AE38",
+        hwid = "ASC505AF-163A-448B-B034-448B448B448B",
+        description = "DJ 5Day"
+    },
+    {
+        key = "LASDIO-ASDWQW-3AE38",
+        hwid = "C5C505AF-163A-448B-B034-CD6BEEF4F652",
+        description = "DJ 10Day"
+    }
+}
