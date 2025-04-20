3a819f8537a5b89ebaa6e6836af91bf5cfd64ce8
RH-o666 <127748418+minute124@users.noreply.github.com>
2025-04-20 16:49:36 +0900

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -1147,28 +1147,12 @@ local themes = {
 		["Object Background"] = Color3.fromRGB(25, 25, 29),
 		["Object Border"] = Color3.fromRGB(35, 35, 39),
 		["Dropdown Option Background"] = Color3.fromRGB(19, 19, 23)
-	},
-
-	AirHub = {
-		["Accent"] = Color3.fromRGB(150, 100, 150),
-		["Window Background"] = Color3.fromRGB(35, 30, 35),
-		["Window Border"] = Color3.fromRGB(50, 45, 50),
-		["Tab Background"] = Color3.fromRGB(25, 20, 25),
-		["Tab Border"] = Color3.fromRGB(65, 45, 65),
-		["Tab Toggle Background"] = Color3.fromRGB(35, 30, 35),
-		["Section Background"] = Color3.fromRGB(25, 20, 25),
-		["Section Border"] = Color3.fromRGB(55, 35, 55),
-		["Text"] = Color3.fromRGB(200, 180, 200),
-		["Disabled Text"] = Color3.fromRGB(120, 100, 120),
-		["Object Background"] = Color3.fromRGB(30, 25, 30),
-		["Object Border"] = Color3.fromRGB(40, 35, 40),
-		["Dropdown Option Background"] = Color3.fromRGB(25, 20, 25)
 	}
 }
 
 local themeobjects = {}
 
-local library = utility.table({theme = table.clone(themes.Midnight), folder = "Megalodonwtf", extension = "cfg", flags = {}, open = false, keybind = Enum.KeyCode.RightShift, mousestate = services.InputService.MouseIconEnabled, cursor = nil, holder = nil, connections = {}}, true)
+local library = utility.table({theme = table.clone(themes.Default), folder = "Megalodonwtf", extension = "cfg", flags = {}, open = false, keybind = Enum.KeyCode.RightShift, mousestate = services.InputService.MouseIconEnabled, cursor = nil, holder = nil, connections = {}}, true)
 local decode = (syn and syn.crypt.base64.decode) or (crypt and crypt.base64decode) or base64_decode
 library.gradient = decode("iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABuSURBVChTxY9BDoAgDASLGD2ReOYNPsR/+BAfroI7hibe9OYmky2wbUPIOdsXdc1f9WMwppQm+SDGBnUvomAQBH49qzhFEag25869ElzaIXDhD4JGbyoEVxUedN8FKwnfmwhucgKICc+pNB1mZhdCdhsa2ky0FAAAAABJRU5ErkJggg==")
 library.utility = utility
@@ -1398,7 +1382,7 @@ function library:SetTheme(theme)
 end
 
 function library:GetThemes()
-	local themes = {"Default", "Midnight", "AirHub"}
+	local themes = {"Default", "Midnight"}
 
 	local folderpath = string.format("%s//themes", self.folder)
 
@@ -2793,7 +2777,7 @@ function library:Load(options)
 	local name = "Megalodon.wtf"
 	local sizeX = options.sizex or 450
 	local sizeY = options.sizey or 500
-	local theme = options.theme and options.theme or "AirHub"
+	local theme = options.theme and options.theme or "Midnight"
 	local overrides = options.themeoverrides or {}
 	local folder = options.folder
 	local extension = options.extension
@@ -3829,4 +3813,3 @@ end
 return library
 
 -- Forked from https://github.com/vozoid/ui-libraries/blob/main/drawing/void/source.lua
--- Edited by Exunys for AirHub V2
